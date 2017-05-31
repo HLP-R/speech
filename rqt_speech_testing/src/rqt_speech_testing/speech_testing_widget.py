d2460bd5e4c6279676a2109a4cc7b774b535b1ed
Ryan Petschek <petschekr@gmail.com>
2017-05-31 12:16:35 -0400

    Implement starting / stopping audio recording in the UI

--- rqt_speech_testing/src/rqt_speech_testing/speech_testing_widget.py
+++ rqt_speech_testing/src/rqt_speech_testing/speech_testing_widget.py
@@ -1,11 +1,16 @@
 import os
+import signal
+import threading
+
 import rospkg
 import rospy
-import signal
-
 from python_qt_binding import loadUi
-from python_qt_binding.QtCore import Qt, qWarning, Signal
-from python_qt_binding.QtGui import QFileDialog, QIcon, QWidget, QMessageBox, QHeaderView, QTreeWidgetItem
+from python_qt_binding.QtCore import Qt, Signal, qWarning
+from python_qt_binding.QtGui import (QFileDialog, QHeaderView, QIcon,
+                                     QMessageBox, QTreeWidgetItem, QWidget)
+
+from hlpr_speech_recognition.speech_recognizer import SpeechRecognizer
+
 
 class SpeechTestWidget(QWidget):
     """
@@ -15,14 +20,38 @@ class SpeechTestWidget(QWidget):
 
     def __init__(self, context):
         super(SpeechTestWidget, self).__init__()
-        ui_file = os.path.join(rospkg.RosPack().get_path("rqt_speech_testing"), "resource", "SpeechTest.ui")
+        ui_file = os.path.join(rospkg.RosPack().get_path(
+            "rqt_speech_testing"), "resource", "SpeechTest.ui")
         loadUi(ui_file, self)
 
         self.setObjectName('SpeechTestUi')
-        self.setWindowTitle(self.windowTitle() + (' (%d)' % context.serial_number()))
+        self.setWindowTitle(self.windowTitle() + (' (%d)' %
+                                                  context.serial_number()))
         # Add widget to the user interface
         context.add_widget(self)
 
-        # Set icons for buttons because they don't persist from Qt creator for some reason
+        self.recognizer = SpeechRecognizer(subnode=True)
+
+        # Set icons for buttons because they don't persist from Qt creator
         self.openLocationButton.setIcon(QIcon.fromTheme("document-open"))
         self.recordButton.setIcon(QIcon.fromTheme("media-record"))
+
+        # Attach event handlers
+        self.openLocationButton.clicked[bool].connect(self.openAudio)
+        self.location.returnPressed.connect(self.loadAudio)
+        self.recordButton.toggled.connect(self.recordAudio)
+
+    def openAudio(self):
+        pass
+
+    def loadAudio(self):
+        pass
+
+    def recordAudio(self, state):
+        if state:
+            threading.Thread(target=self.recordAudioThread).start()
+        else:
+            self.recognizer.end_rec()
+
+    def recordAudioThread(self):
+        self.recognizer.begin_rec()
